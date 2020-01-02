840065c01ea0c0bdb06436ff2db5a8dd98554326
IsoArm <54005798+IsoArm@users.noreply.github.com>
2020-01-02 15:57:19 +0400

    Update Acronyms.R

--- Acronyms.R
+++ Acronyms.R
@@ -2,19 +2,22 @@ library(tidyverse)
 library(stringr)
 
 mydata = tibble(
-  text = c("ARA vonC EM, qezanic zzvUM", "hh fgr rrr, grt")
+  text = c("ARA vonC EM, qezanic zzvUM", "hh fgr rrr, grt", "AMF fftb RRTVvRF")
   )
 my_list <- str_extract_all(mydata$text, "\\b[A-Za-z]*[A-Z]{2,}[A-Za-z]*\\b")
 list_len<- length(my_list)
 num_acronyms = c()
 acronyms = c()
 for (i  in 1:list_len){
-  num_acronyms[i] = length(my_list[[i]])
-  acronyms[i] = str_c(my_list[[i]], collapse = ", " )
+    if(!is.null(my_list[[i]])) {acronyms[i] <- str_c(my_list[[i]], collapse = ", " )
+                              num_acronyms[i] <- length(my_list[[i]])
+  } else{
+    num_acronyms[i] <- 0
+    acronyms[i] <- "No match"
+  }
 }
-mydata <- mutate(mydata, num_acronyms = num_acronyms) %>%
+mydata_new <- mutate(mydata, num_acronyms = num_acronyms) %>%
               mutate(acronyms = acronyms)
 
-mydata$acronyms[mydata_new$num_acronyms == 0] <- "NA"
-
+#mydata$acronyms[mydata$num_acronyms == 0] <- "NA"
 mydata_new
